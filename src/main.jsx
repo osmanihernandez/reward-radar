80f2797dd2328a6f4bf4ecd03fa88f886c5c52a0
Osmani Hernandez <osmanihernandezperez@gmail.com>
2026-03-25 16:53:12 -0400

    fix routing for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,17 @@ import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { BrowserRouter } from "react-router";
+// import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router-dom";
 import theme from "./theme.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
-    </BrowserRouter>
-  </StrictMode>
+    </HashRouter>
+  </StrictMode>,
 );
